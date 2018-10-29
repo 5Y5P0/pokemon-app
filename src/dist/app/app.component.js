80af34377351f2f7f905aed7bdbc257b07bf876a
FOUCHER SEBASTIEN (BPGO) <SEBASTIEN.FOUCHER@bpgo.fr>
2018-10-29 17:54:47 +0100

    Chapitre 6 : Les composants

--- src/dist/app/app.component.js
+++ src/dist/app/app.component.js
@@ -7,14 +7,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var mock_pokemons_1 = require("./mock-pokemons");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.pokemons = null;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.pokemons = mock_pokemons_1.POKEMONS;
+    };
+    AppComponent.prototype.selectPokemon = function (pokemon) {
+        console.log('Vous avez selectionné ' + pokemon.name);
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'pokemon-app',
-            template: '<h1>Hello {{name}} !</h1>'
+            template: '<h1>Pokémons</h1>'
         })
     ], AppComponent);
     return AppComponent;
